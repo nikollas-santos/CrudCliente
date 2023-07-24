4328e498c50404e27494825552ae1520d2570f0c
Nikollas Santos <nikollasjunior@hotmail.com>
2023-07-24 19:45:55 -0300

    Melhoria ao deletar um usuario nao encontrado

--- src/services/cliente/DeletarClienteService.ts
+++ src/services/cliente/DeletarClienteService.ts
@@ -1,20 +1,31 @@
 import prismaClient from "../../prisma";
 
 interface ClientRequest {
-    id:string
+    id: string;
 }
 
 class DeletarClienteService {
-    async execute({id}:ClientRequest){
-        const removecliete = await prismaClient.cliente.delete({
-            where:{
-                id:id
-            }
-        })
+    async execute({ id }: ClientRequest) {
+        // Verificar se o cliente com o ID fornecido existe no banco
+        const clienteExistente = await prismaClient.cliente.findUnique({
+            where: {
+                id: id,
+            },
+        });
+    // Lançar um erro caso o cliente não exista
+        if (!clienteExistente) {
+            throw new Error("Cliente não encontrado"); // Lançar um erro caso o cliente não exista
+        }
 
-        return removecliete
+        // Se o cliente existir, excluir
+        const removecliente = await prismaClient.cliente.delete({
+            where: {
+                id: id,
+            },
+        });
 
+        return removecliente;
     }
 }
 
-export {DeletarClienteService}
+export { DeletarClienteService };
