edf2019352e787e6f93358fec9b5e71cd62c4789
Nikollas Santos <nikollasjunior@hotmail.com>
2023-07-24 20:13:50 -0300

    Selecionar id, name e email na hora de excluir

--- src/services/cliente/DeletarClienteService.ts
+++ src/services/cliente/DeletarClienteService.ts
@@ -5,23 +5,28 @@ interface ClientRequest {
 }
 
 class DeletarClienteService {
-    async execute({ id }: ClientRequest) {
+    async execute(objeto: ClientRequest) {
         // Verificar se o cliente com o ID fornecido existe no banco
         const clienteExistente = await prismaClient.cliente.findUnique({
             where: {
-                id: id,
+                id: objeto.id,
             },
         });
     // Lançar um erro caso o cliente não exista
         if (!clienteExistente) {
-            throw new Error("Cliente não encontrado"); // Lançar um erro caso o cliente não exista
+            throw new Error("Cliente não encontrado na base de dados"); // Lançar um erro caso o cliente não exista
         }
 
         // Se o cliente existir, excluir
         const removecliente = await prismaClient.cliente.delete({
             where: {
-                id: id,
+                id:objeto.id,
             },
+            select:{
+                id:false,
+                name:true,
+                email:false
+            }
         });
 
         return removecliente;
