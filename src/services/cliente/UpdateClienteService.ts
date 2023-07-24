eb34999bd0c95f49c465c5a6cb154bf09440ec6a
Nikollas Santos <nikollasjunior@hotmail.com>
2023-07-24 16:16:53 -0300

    Melhorias ao atualizar um usuario

--- src/services/cliente/UpdateClienteService.ts
+++ src/services/cliente/UpdateClienteService.ts
@@ -1,26 +1,38 @@
 import prismaClient from "../../prisma";
 
-interface UpdateClienteRequest{
-    id: string,
-    email:string,
-    name:string,
-    endereco:string
+interface UpdateClienteRequest {
+  id: string,
+  email: string,
+  name: string,
+  endereco: string
 }
 
 class UpdateClienteService {
-    async execute (objeto:UpdateClienteRequest){
-        const cliente = await prismaClient.cliente.update({
-            where:{
-                id:objeto.id
-            },
-            data:{
-                endereco:objeto.endereco,
-                name:objeto.name,
-                email:objeto.email
-            }
-        })
-        return cliente
+  async execute(objeto: UpdateClienteRequest) {
+    const clientAlreadyExists = await prismaClient.cliente.findFirst({
+      where: {
+        email: objeto.email,
+        id: { not: objeto.id } // Check if the client exists with the same email, but a different id
+      }
+    });
+
+    if (clientAlreadyExists) {
+      throw new Error("Cliente already exists");
     }
+
+    const cliente = await prismaClient.cliente.update({
+      where: {
+        id: objeto.id
+      },
+      data: {
+        endereco: objeto.endereco,
+        name: objeto.name,
+        email: objeto.email
+      }
+    });
+
+    return cliente;
+  }
 }
 
-export {UpdateClienteService}
+export { UpdateClienteService };
